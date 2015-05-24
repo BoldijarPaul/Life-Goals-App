524e76af9371545ef786a816179e209553c1a476
Boldijar Paul <paul.bv96@yahoo.com>
2015-05-24 21:31:06 +0300

    Updated client Now if the token expires you are logged out.

---
+++ src/com/lifegoals/app/activities/AppApplication.java
@@ -0,0 +1,43 @@
+package com.lifegoals.app.activities;
+
+import android.app.Application;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+import com.lifegoals.app.client.management.AppContext;
+import com.lifegoals.app.client.management.ContextRequestListener;
+
+public class AppApplication extends Application implements
+		ContextRequestListener {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		AppContext.setOnRequestListener(this);
+	}
+
+	@Override
+	public void onGetStatusCode(int code) {
+		if (code == 401) {
+			/* not authorized */
+			showToast("Your session expired! Please login again!");
+			Intent loginIntent = new Intent(this, HomeActivity.class);
+			loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+					| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+			startActivity(loginIntent);
+
+		}
+
+	}
+
+	private void showToast(final String message) {
+		new Handler(Looper.getMainLooper()).post(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(getApplicationContext(), message,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+}
