00f50e9149a30c8c926ac57af7fb26c12c27bce1
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-29 13:42:06 +0300

    removed eclipse files

--- src/com/lifegoals/app/adapters/owngoals/OwnGoalsAdapterListener.java
+++
@@ -1,8 +0,0 @@
-package com.lifegoals.app.adapters.owngoals;
-
-import com.lifegoals.app.entities.Goal;
-
-
-public interface OwnGoalsAdapterListener {
-	public void onDeleteOwnGoal(Goal goal);
-}
--- src/com/lifegoals/app/adapters/savedgoals/SavedGoalAdapterListener.java
+++
@@ -1,7 +0,0 @@
-package com.lifegoals.app.adapters.savedgoals;
-
-import com.lifegoals.app.entities.SavedGoal;
-
-public interface SavedGoalAdapterListener {
-	public void onSavedGoalClicked(SavedGoal savedGoal);
-}
